e73fca348632c1fee84bf83343eb673db054478d
byeongjuPark <bottlepark28@gmail.com>
2024-02-08 21:01:13 +0900

    240208

---
+++ express/express_study1.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) => {
+    res.send('hello world');
+});
+
+app.listen(8080, ()=>{
+    console.log('8080포트에서 서버 실행중');
+})
